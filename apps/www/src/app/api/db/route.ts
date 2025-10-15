aff4ca4cde70d0a0d9c082e364d36a2451656b06
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 21:24:42 +0100

    .

--- apps/www/src/app/api/db/route.ts
+++ apps/www/src/app/api/db/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@next16/db";
 import { type NextRequest, NextResponse } from "next/server";
-
+export const runtime = "nodejs";
 export async function GET(req: NextRequest) {
   const data = await prisma.user.count({});
   return NextResponse.json({
